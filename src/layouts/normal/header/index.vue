0fd2fe0330205f834d44f1e974867e97d0e3d461
Jutipong <techcom.ec20@gmail.com>
2024-05-01 13:24:58 +0700

    Remove unused layout components

--- src/layouts/normal/header/index.vue
+++ src/layouts/normal/header/index.vue
@@ -1,16 +1,8 @@
-<!--------------------------------
- - @Author: Ronnie Zhang
- - @LastEditor: Ronnie Zhang
- - @LastEditTime: 2023/12/16 18:52:48
- - @Email: zclzone@outlook.com
- - Copyright © 2023 Ronnie Zhang(大脸怪) | https://isme.top
- --------------------------------->
-
 <template>
   <AppCard class="flex items-center px-12" border-b="1px solid light_border dark:dark_border">
     <MenuCollapse />
 
-    <!-- <AppTab class="w-0 flex-1 px-12" /> -->
+    <div class="w-0 flex-1 px-12" />
 
     <span class="mx-6 opacity-20">|</span>
 
